f6619a441c321bd9fb186479b212c24998530e71
Al_sakhen <ahmadalsakhen36@gmail.com>
2022-06-21 21:17:00 +0300

    o

--- addToCart.php
+++ addToCart.php
@@ -4,11 +4,10 @@ session_start();
 require_once("handlers/connect.php");
 require_once("handlers/db.php");
 
+
 $productID=$_GET['id'];
-
 $product = getWhere('products','id='.$productID);
-
-
+$user_id = $_SESSION['user'][0]['id'];
 
 
 $product[0]['qtyUser'] = 1;
@@ -16,17 +15,17 @@ $product[0]['qtyUser'] = 1;
 
 if(isset($_SESSION['cart'])){
     
-  $_SESSION['cart'][$productID] = $product;
+  $_SESSION['cart'][$productID][$user_id] = $product;
 }else{
     $_SESSION['cart'] = [];
-    $_SESSION['cart'][$productID] = $product;
+    $_SESSION['cart'][$productID][$user_id]  = $product;
 }
-
+// echo "<pre>";
+// print_r($_SESSION['cart']);
+// die;
 
 $_SESSION['successAddToCart'] ="add to cart ";
-echo '<pre>';
-print_r($_SESSION['cart']);
-echo '</pre>';
+
 
 header("Location: Products.php");
 ?>
